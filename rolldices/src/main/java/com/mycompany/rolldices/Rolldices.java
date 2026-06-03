264c08c3501162108605240c547a3882c51aad8d
preciado-002 <arleypreciado0102@gmail.com>
2026-06-02 22:29:03 -0500

    Agregar carpeta rolldices en minusculas

---
+++ rolldices/src/main/java/com/mycompany/rolldices/Rolldices.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rolldices;
+
+/**
+ *
+ * @author Personal
+ */
+public class Rolldices {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
